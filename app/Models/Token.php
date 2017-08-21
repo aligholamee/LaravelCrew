b790c73f4c5eff55d16556bd6a5c39c1ec6d59c3
aligholamee <aligholami7596@gmail.com>
2017-08-21 09:55:07 +0330

    Token model relationships were added

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -9,4 +9,13 @@ class Token extends Model
     // Configure the model
     protected $table = 'token';
     public $primary_key = 'uuid';
+
+    // Define relationships here
+    public function generator_user() {
+      $this -> belongsTo('user');
+    }
+
+    public function consumer_user() {
+      $this -> belongsTo('user');
+    }
 }
