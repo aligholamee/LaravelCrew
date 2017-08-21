af5368c4881fea6fcbd8842b4d5886fd88dcdf3c
aligholamee <aligholami7596@gmail.com>
2017-08-21 09:52:08 +0330

    Role model was fixed

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,4 +9,10 @@ class Role extends Model
     // Configure the model
     protected $table = 'role';
     public $primary_key = 'uuid';
+
+    // Define relationships here
+    public function role_permission() {
+      // Each role belongs to one role_permission record of it's table
+      $this -> belongsTo('role_permission')
+    }
 }
