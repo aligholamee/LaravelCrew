d94a6368233d484e45ec80add6625958f41cac48
aligholamee <aligholami7596@gmail.com>
2017-08-21 10:09:18 +0330

    User model was configured

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,4 +9,31 @@ class User extends Model
     // Configure the model
     protected $table = 'user';
     public $primary_key = 'uuid';
+
+    // Define relationships here
+    public function house() {
+      $this -> belongsTo('house');
+    }
+
+    public function user_house() {
+      $this -> BelongsTo('user_house');
+    }
+
+    public function user_door() {
+      $this -> BelongsTo('user_house');
+    }
+
+    public function role_permission() {
+      $this -> hasOne('role_permission');
+    }
+
+    public function token1() {
+      $this -> belongsTo('token');
+    }
+
+    public function token2() {
+      $this -> belongsTo('token');
+    }
+
+
 }
