c3168e6417fdf667d4a40dc4131347f45a5e45cc
aligholamee <aligholami7596@gmail.com>
2017-08-21 09:58:53 +0330

    user_door model was updated

--- app/Models/User_Door.php
+++ app/Models/User_Door.php
@@ -9,4 +9,13 @@ class User_Door extends Model
     // Configure the model
     protected $table = 'user_door';
     public $primary_key = 'uuid';
+
+    // Define relationships here
+    public function user() {
+      $this -> hasOne('user');
+    }
+
+    public function door() {
+      $this -> hasOne('door');
+    }
 }
