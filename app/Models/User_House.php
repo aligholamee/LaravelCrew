4f5636a121782c5c802ebe0673d2c5b1b5b806bc
aligholamee <aligholami7596@gmail.com>
2017-08-21 09:59:45 +0330

    user_house model was updated

--- app/Models/User_House.php
+++ app/Models/User_House.php
@@ -9,4 +9,13 @@ class User_House extends Model
     // Configure the model
     protected $table = 'user_house';
     public $primary_key = 'uuid';
+
+    // Define relationships here
+    public function user() {
+      $this -> hasOne('user');
+    }
+
+    public function house() {
+      $this -> hasOne('house');
+    }
 }
