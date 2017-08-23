a68bb92e77b1a980f2e0831428bb51ab5e717a45
aligholamee <aligholami7596@gmail.com>
2017-08-23 06:16:47 +0330

    Admin panel was customized a bit

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -12,14 +12,14 @@ return [
     */
 
     // Project name. Shown in the breadcrumbs and a few other places.
-    'project_name' => 'Backpack',
+    'project_name' => 'Arda Security',
 
     // Menu logos
-    'logo_lg'   => '<b>Back</b>pack',
+    'logo_lg'   => '<b>Arda</b>Security',
     'logo_mini' => '<b>B</b>p',
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'Ali Gholami',
 
     // Developer website. Link in footer.
     'developer_link' => 'http://tabacitu.ro',
@@ -28,7 +28,7 @@ return [
     'show_powered_by' => true,
 
     // The AdminLTE skin. Affects menu color and primary/secondary colors used throughout the application.
-    'skin' => 'skin-purple',
+    'skin' => 'skin-green',
     // Options: skin-black, skin-blue, skin-purple, skin-red, skin-yellow, skin-green, skin-blue-light, skin-black-light, skin-purple-light, skin-green-light, skin-red-light, skin-yellow-light
 
     // Date & Datetime Format Syntax: https://github.com/jenssegers/date#usage
