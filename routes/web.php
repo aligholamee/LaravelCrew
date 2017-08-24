2b5e4c7550d0eb2ae518c1fafd82b795562abf19
aligholamee <aligholami7596@gmail.com>
2017-08-24 15:42:34 +0330

    revert changes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-/** CATCH-ALL ROUTE for Backpack/PageManager - needs to be at the end of your routes.php file  **/
-Route::get('{page}/{subs?}', ['uses' => 'PageController@index'])
-    ->where(['page' => '^((?!admin).)*$', 'subs' => '.*']);
