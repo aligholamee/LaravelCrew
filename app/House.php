7b587deaf857cb5159e716981005818969a679f9
aligholamee <aligholami7596@gmail.com>
2017-08-24 17:00:21 +0330

    House model was added

---
+++ app/House.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class House extends Model
+{
+     /*
+    |--------------------------------------------------------------------------
+    | GLOBAL VARIABLES
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    // protected $table = 'houses';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+     protected $primaryKey = 'uuid';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var boolean
+     */
+    // public $timestamps = false;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    // protected $guarded = ['id'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+     protected $fillable = ['name', 'comment', 'address', 'user_uuid'];
+
+    /**
+     * The attributes that should be hidden for arrays
+     *
+     * @var array
+     */
+    // protected $hidden = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    // protected $dates = [];
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTIONS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+    public function user() {
+      return $this->hasOne('User');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | SCOPES
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESORS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | MUTATORS
+    |--------------------------------------------------------------------------
+    */
+}
