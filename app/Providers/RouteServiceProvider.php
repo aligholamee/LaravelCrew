cc8d9865a0cf529c9a86eaba9fa7078654dff287
aligholamee <aligholami7596@gmail.com>
2017-08-26 11:38:53 +0330

    The new route was registered to the RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapAdminRoutes();
+
         //
     }
 
@@ -69,5 +71,20 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Define the "admin / CRUD" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapAdminRoutes()
+    {
+      Route::middleware(['web', 'admin'])
+     ->prefix('admin') // or use the prefix from CRUD config
+     ->namespace($this->namespace.'\Admin')
+     ->group(base_path('routes/admin.php'));
     }
 }
