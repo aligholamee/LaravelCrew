873c9befc91287bce84a7db591419cfbeb6f9f2a
aligholamee <aligholami7596@gmail.com>
2017-08-28 14:50:04 +0330

    Token route was added

--- routes/admin.php
+++ routes/admin.php
@@ -3,3 +3,4 @@
 // Backpack\CRUD: Define the resources for the entities you want to CRUD.
 CRUD::resource('House/', 'HouseCrudController');
 CRUD::resource('Door/', 'DoorCrudController');
+CRUD::resource('Token/', 'TokenCrudController');
