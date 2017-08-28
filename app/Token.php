801875d136d2f88363a8498627dea49d0329eb30
aligholamee <aligholami7596@gmail.com>
2017-08-28 14:12:41 +0330

    Add token model and it's relationships

---
+++ app/Token.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App;
+use Alsofronie\Uuid\UuidModelTrait;
+use Illuminate\Database\Eloquent\Model;
+use Backpack\CRUD\CrudTrait;
+
+class Token extends Model
+{
+    // Optimized uuid
+    private static $uuidOptimization = true;
+
+     /*
+    |--------------------------------------------------------------------------
+    | GLOBAL VARIABLES
+    |--------------------------------------------------------------------------
+    */
+    use UuidModelTrait;
+    use CrudTrait;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+     protected $table = 'door';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+     protected $primaryKey = 'uuid';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var boolean
+     */
+    // public $timestamps = false;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    // protected $guarded = ['id'];
+
+    /**
+     * The attributes that are mass assignable.
+     *  
+     * @var array
+     */
+     protected $fillable = ['start_date', 'end_date', 'generator_uuid', 'consumer_uuid'];
+
+    /**
+     * The attributes that should be hidden for arrays
+     *
+     * @var array
+     */
+    // protected $hidden = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    // protected $dates = [];
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTIONS
+    |--------------------------------------------------------------------------
+    */
+  
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+
+    public function generator_user() {
+        return $this->belongsTo('User');
+    }
+
+    public function consumer_user() {
+        return $this->belongsTo('User');
+    }
+
+    public function door() {
+      return $this->belongsTo('Door');
+    }
+
+    public function door_token() {
+      return $this->belongsTo('door_token');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | SCOPES
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESORS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | MUTATORS
+    |--------------------------------------------------------------------------
+    */
+}
