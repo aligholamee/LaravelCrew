35a77a62308ec3aaffd233342e6a8f90fd7709af
aligholamee <aligholami7596@gmail.com>
2017-08-28 13:52:24 +0330

    Token route

--- resources/views/vendor/backpack/crud/buttons/update.blade.php
+++ resources/views/vendor/backpack/crud/buttons/update.blade.php
@@ -6,7 +6,7 @@
 			<a href="{{ url(config('backpack.base.route_prefix'), 'Door') }}?house_uuid={{$entry->uuid}}" class="btn btn-xs btn-default"><i class="fa fa-edit"></i> Manage Doors </a>
 		@endif 	
 		@if ($crud->entity == 'Door')
-		<a href="{{ url(config('backpack.base.route_prefix'), 'Door') }}?house_uuid={{$entry->uuid}}" class="btn btn-xs btn-default"><i class="fa fa-edit"></i> Generate Token </a>
+		<a href="{{ url(config('backpack.base.route_prefix'), 'Token') }}?door_uuid={{$entry->uuid}}" class="btn btn-xs btn-default"><i class="fa fa-edit"></i> Generate Token </a>
 		@endif
 		
 	@else
