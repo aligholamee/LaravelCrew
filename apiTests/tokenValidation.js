810e455896a1e65903dc56f7372ca1dc6aa0e3f4
aligholamee <aligholami7596@gmail.com>
2017-09-03 11:46:03 +0330

    Improve token validation test

--- apiTests/tokenValidation.js
+++ apiTests/tokenValidation.js
@@ -1,6 +1,9 @@
-var requestedValue = '641509';
-var requestedDoorUuid = 'cb0dc199-f45f-4522-be7c-ee4add1e9e6c';
-var generatedToken = requestedValue + requestedDoorUuid;
+var value = '641509';
+var doorUuid = 'cb0dc199-f45f-4522-be7c-ee4add1e9e6c';
+var generatedToken = [];
+generatedToken[0] = value;
+generatedToken[1] = doorUuid;
+
 postman.setEnvironmentVariable('token',base64url(generatedToken));
 
 function base64url(input) {
