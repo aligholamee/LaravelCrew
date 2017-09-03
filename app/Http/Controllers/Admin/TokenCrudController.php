79852e09600acf17fee8c665bc271b8f9be097fa
aligholamee <aligholami7596@gmail.com>
2017-09-03 05:55:44 +0330

    Validation was completed

--- app/Http/Controllers/Admin/TokenCrudController.php
+++ app/Http/Controllers/Admin/TokenCrudController.php
@@ -157,8 +157,12 @@ class TokenCrudController extends CrudController
 
         // Query database for validaton
         // Returns a 2D array
-        $validationResult = App\Token::where('door_uuid', $deserializedToken[0])->where('value', $deserializedToken[1])->get();
-        
+        $validationResultLength = App\Token::where('door_uuid', $deserializedToken[0])->where('value', $deserializedToken[1])->whereRaw('now() > start_date AND now() < end_date')->count();
+        if($validationResultLength == 0)
+            // Invalid 
+            return false;
+        else 
+            return true;
    
     }
 }
